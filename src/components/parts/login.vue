0107c684ca9b7a647d3d9db272a4be40ebfac697
noguchi-makito <noguchi.4424@gmail.com>
2020-02-14 00:35:04 +0900

    ログイン部分作成

--- src/components/parts/login.vue
+++ src/components/parts/login.vue
@@ -2,31 +2,45 @@
   <div>
     <ul class="login">
       <li class="login__logo">
-        <img src="../../assets/img/mtrad_logo.jpg" alt="ロゴ" width="100%">
+        <img src="../../assets/img/mtrad_logo.jpg" alt="ロゴ" width="100%" />
+      </li>
+      <li class="login__email">
+        <span class="login__email--icon">
+          <font-awesome-icon icon="envelope" />
+        </span>
+        <input type="text" v-model="email" placeholder="アドレスを入力してください" />
       </li>
       <li class="login__password">
-        <span class="login__password--icon"><font-awesome-icon icon="lock" /></span>
-        <input type="password"  placeholder="PASSWORDを入力してください。">
-      </li>
-      <li class="login__button">
-        <router-link to="/date" class="button">
-          Login
-        </router-link>
+        <span class="login__password--icon">
+          <font-awesome-icon icon="lock" />
+        </span>
+        <input type="password" v-model="password" placeholder="PASSWORDを入力してください。" />
       </li>
     </ul>
-
+    <button type="button" class="button" @click="login()">Login</button>
   </div>
 </template>
 
 <script>
 /* ログインページcss読み込み */
-import '../../assets/scss/login.scss'
+import "../../assets/scss/login.scss";
 export default {
-
-}
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    login: function() {
+      alert(
+        "パスワードは" + this.password + "\n" + "メールアドレスは" + this.email
+      );
+    }
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
